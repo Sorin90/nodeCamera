ad89e381c3209a27fe53a05dad7ab86a4af13931
Sorin90 <sorinlupo@gmail.com>
2022-10-10 23:55:18 +0200

    Multifile and srvcfg

--- app.js
+++ app.js
@@ -3,11 +3,9 @@ const fs = require('fs');
 const path = require('path');
 const url = require('url');
 const formidable = require('formidable');
-//import formidable from 'formidable';
-//ciaone
-
 const app = express();
-const port = process.env.PORT || 3000;
+//const port = process.env.PORT || 3000;
+var port = "";
 const basePath = __dirname;
 const uploadFolder = '/caricati/';
 
@@ -16,26 +14,47 @@ app.get('/', function (req, res) {
 	res.sendFile(path.join(basePath, '/index.html'));
 });
 
+fs.readFile('srvcfg.json', 'utf8', (err, data) => {
+	if (err) {
+	  console.error(err);
+	  return;
+	}
+	const srvcfg = JSON.parse(data)
+	 port = JSON.parse(data).port;
+	 app.listen(port);
+	console.log('Server started at http://localhost:' + port);
+  });
+
 
 // sendFile will go here
 app.post('/fileupload', function (req, res) {
 	var q = url.parse(req.url, true);
-	console.log(q);
-	var form = new formidable.IncomingForm();
+	var form = formidable({ multiples: true });
 	form.parse(req, function (err, fields, files) {
-		console.log(files)
-		var oldpath = files.fileupload.filepath;
-		var newpath = basePath + uploadFolder + q.query.folder.substring(0, 3) + '/' + q.query.folder.substring(3) + '/';
-		if (!fs.existsSync(newpath)) {
-			fs.mkdirSync(newpath, { recursive: true });
+		var arr = []
+		let c = 0;
+		// in case of single file. Use with formidable v2 maybe fixed with v3
+		if (files.fileupload.size > 0) {
+			var tmp = Object.assign({}, files);
+			files = {}
+			files.fileupload = [tmp.fileupload];
 		}
-		fs.rename(oldpath, newpath + files.fileupload.originalFilename, function (err) {
-			if (err) throw err;
-			res.redirect('/?message=loaded:' + files.fileupload.originalFilename);
-		});
+		for (let i = 0; i < files.fileupload.length; i++) {
+			var oldpath = files.fileupload[i].filepath;
+			var newpath = basePath + uploadFolder + q.query.folder.substring(0, 3) + '/' + q.query.folder.substring(3) + '/';
+			if (!fs.existsSync(newpath)) {
+				fs.mkdirSync(newpath, { recursive: true });
+			}
+			fs.rename(oldpath, newpath + files.fileupload[i].originalFilename, function (err) {
+				//console.log(files.fileupload.length + '--' + c)
+				if (err == null)
+					c++;
+				else throw err
+				if (files.fileupload.length == c)
+					res.redirect('/?message=caricate:' + c + ' images');
+			});
+		}
 	});
 });
 
 
-app.listen(port);
-console.log('Server started at http://localhost:' + port);
